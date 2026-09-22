60a7df80115f6b4ed59a7f155ddcdbbce598fb8e
Om chaudhary <167326628+omchaudhary007@users.noreply.github.com>
2026-09-22 14:50:44 +0530

    Update iframe source URL in page.tsx

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -8,7 +8,7 @@ export default function Resume() {
       <div className="w-full my-6 border-b-2 border-foreground/10"></div>
       <iframe
         className="h-100 md:h-screen w-full"
-        src="https://drive.google.com/file/d/1uqmT6uNdFcLPu_JumUpfU30wbtHe7fMa/preview"
+        src="https://drive.google.com/file/d/1XrnkCDAd1-GtbNsgElw0zBwAE36Ouw2-/view"
       ></iframe>
     </div>
   );
